627c84714a55d3824703c88bac98753d48fadf38
YiHsuan8 <ftyhnbv243@gmail.com>
2022-10-25 20:33:33 +0800

    RWD

--- Code/js/all.js
+++ Code/js/all.js
@@ -55,3 +55,18 @@ $(window).scroll(function () {
     $(".toTop").addClass("hide");
   }
 });
+
+// accordion
+
+$(function () {
+  $(".accBtn").click(function (e) {
+    e.preventDefault();
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this).next().stop().slideUp(300);
+    } else {
+      $(this).addClass("active");
+      $(this).next().stop().slideDown(300);
+    }
+  });
+});
